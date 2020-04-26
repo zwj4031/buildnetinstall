388c5f39bca2010f034278072bea0443fd918110
root <root@DESKTOP-KIQQ73C.localdomain>
2020-04-26 10:10:55 +0800

    add hfs.exe

--- startpxe.bat
+++ startpxe.bat
@@ -1,5 +1,5 @@
 taskkill /f /im hfs.exe
-start "" /min %~dp0\bin\hfs.exe -c active=yes -a myhfs.ini
+start "" /min %~dp0\bin\hfs.exe -c active=yes -a %~dp0\bin\myhfs.ini
 for /f %%a in ('dir /b/a-d  %~dp0\*.*') do start "" /min %~dp0\bin\hfs.exe %%a
 start "" /min %~dp0\bin\hfs.exe  %~dp0\app
 exit
